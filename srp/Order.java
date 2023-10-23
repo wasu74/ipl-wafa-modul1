4170e63d6ead622f95a1f8a2c009e23e9ee3203c
LAPTOP-8NJEUAH9\user <wafasubaga.05@gmail.com>
2023-10-23 22:54:17 +0700

    re format code

--- srp/Order.java
+++ srp/Order.java
@@ -2,31 +2,31 @@ package srp;
 
 class Order {
     void calculateTotalSum() {
-        /..../
+        /* ... */
     }
 
     void getItems() {
-        /..../
+        /* ... */
     }
 
     void getItemsCount() {
-        /..../
+        /* ... */
     }
 
     void getItems(Item item) {
-        /..../
+        /* ... */
     }
 
     void deleteItem(Item item) {
-        /..../
+        /* ... */
     }
 
     void printOrder() {
-        /..../
+        /* ... */
     }
             
     void showOrder(Item item) {
-        /..../
+        /* ... */
     }
 
     void getDailyHistory() {
