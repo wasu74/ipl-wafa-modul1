5b730aa6e5c8908fc6931ff8101e147b3814a354
LAPTOP-8NJEUAH9\user <wafasubaga.05@gmail.com>
2023-10-23 21:25:34 +0700

    menambah contoh srp

---
+++ srp/Order.java
@@ -0,0 +1,38 @@
+package srp;
+
+class Order {
+    void calculateTotalSum() {
+        /..../
+    }
+
+    void getItems() {
+        /..../
+    }
+
+    void getItemsCount() {
+        /..../
+    }
+
+    void getItems(Item item) {
+        /..../
+    }
+
+    void deleteItem(Item item) {
+        /..../
+    }
+
+    void printOrder() {
+        /..../
+    }
+            
+    void showOrder(Item item) {
+        /..../
+    }
+
+    void getDailyHistory() {
+    }
+
+    void getMonthlyHistory() {
+    }
+
+}
